8183c85a523e704a15f88d4df0ccfccedc9404e3
ABINAYA M <132118365+Abinaya-M123@users.noreply.github.com>
2025-03-11 09:54:30 +0530

    restaurant

---
+++ sample.js
@@ -0,0 +1,21 @@
+console.log("Hello world");
+var name="XXX"
+console.log(name)
+function display(){
+    let num=10
+    console.log(num)
+}
+//console.log(num)
+display()
+num1=30
+var ans=(num1>10)?"yes":"no";
+console.log(ans)
+var employee={
+    names:"xxx",
+    id:1,
+    fun:function show(){
+        console.log("Employee Name : "+ this.names)
+    }
+}
+employee.fun()
+//employee.fun()
